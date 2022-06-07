29b9675bbaa8f482b603bb1fea7fe32d98eba6a8
harle <harleyharris05@hotmail.com>
2022-06-07 23:48:29 +0100

    created extracting tactical setup function.

--- functions.js
+++ functions.js
@@ -4,7 +4,7 @@ export function sum(a, b) {
 
 // Season ID
 
-let season_id = []
+let season_ids = []
 
 export function extract_season_ids(competitions_data) {
 
@@ -12,13 +12,60 @@ export function extract_season_ids(competitions_data) {
     
     for (const competition_object of competitions_data) {
         if (competition_object['competition_name'] === 'Champions League') {
-            season_id.push(competition_object['season_id'])
+            season_ids.push(competition_object['season_id'])
         }
     }
 
-    season_id.pop() // removes 76 season id, which contains no data
-    return season_id;
+    season_ids.pop() // removes 76 season id, which contains no data
     
+    const random_index = Math.floor(Math.random() * season_ids.length);
+    
+    const random_season_id = season_ids[random_index];
+
+
+    return random_season_id;
+}
+
+export function extract_tactical_setups(event_object) {
+    
+    const [home_team_tactical_data, away_team_tactical_data] = [event_object[0], event_object[1]];
+    const [home_team_formation, away_team_formation] = [home_team_tactical_data['tactics']['formation'], away_team_tactical_data['tactics']['formation']];
+    const [home_team_lineup_object, away_team_lineup_object] = [home_team_tactical_data['tactics']['lineup'], away_team_tactical_data['tactics']['lineup']];
+
+    function generate_lineups(lineup_object) {
+
+        const extracted_lineup = []
+
+        for (const data of lineup_object) {
+
+            const player_name = data['player']['name'];
+            const player_number = data['jersey_number'].toString();
+
+            const player_position = data['position']['name'];
+            const player_position_split = player_position.split(" ");
+            let player_position_initials = '';
+    
+            for (const word of player_position_split) {
+                if (word === "Goalkeeper") {
+                    player_position_initials += 'GK';
+                } else {
+                    player_position_initials += word[0];
+                }
+            }
+    
+            extracted_lineup.push([player_number, player_name, player_position_initials])
+
+        }
+
+        return extracted_lineup;
+
+    }
+
+    const home_team_lineup_extracted = generate_lineups(home_team_lineup_object);
+    const away_team_lineup_extracted = generate_lineups(away_team_lineup_object);     
+
+    return [home_team_formation, away_team_formation, home_team_lineup_extracted, away_team_lineup_extracted];
+     
 }
 
 
--- script.js
+++ script.js
@@ -2,8 +2,7 @@ const container = document.querySelector('.container');
 
 // Imported functions
 
-import {sum} from './functions.js'
-import {extract_season_ids} from './functions.js'
+import {extract_season_ids, extract_tactical_setups} from './functions.js'
 
 // API endpoints
 
@@ -11,17 +10,49 @@ const champions_league_id = 16;
 const competitions_url = 'https://raw.githubusercontent.com/statsbomb/open-data/master/data/competitions.json'
 
 async function getData() {
-    const response = await fetch(competitions_url);
-    const competitions_data = await response.json();
+
+    // Make competition data get request
+
+    const competitions_response = await fetch(competitions_url);
+    const competitions_data = await competitions_response.json();
     console.log(competitions_data);
     
-    // const total = sum(1,1);
-    // container.textContent = total; 
+    // Generate a random season id
 
-    const season_ids = extract_season_ids(competitions_data);
-    console.log(season_ids);
+    const random_season_id = extract_season_ids(competitions_data);
     
-}
+    // Generate Match id - Using champions_league_id + random_season_id
+
+    const match_url = `https://raw.githubusercontent.com/statsbomb/open-data/master/data/matches/${champions_league_id}/${random_season_id}.json`
+    const match_response = await fetch(match_url);
+    const match_object = await match_response.json();
+
+    // Extract match metadata from match object
+
+    const match_id = match_object[0]['match_id'];
+    const match_date = match_object[0]['match_date'];
+    const home_team = match_object[0]['home_team']['home_team_name'];
+    const away_team = match_object[0]['away_team']['away_team_name'];
+    const home_score = match_object[0]['home_score'];
+    const away_score = match_object[0]['away_score'];
+
+    console.log(match_date, home_team, away_team, home_score, away_score);
+    
+
+    // Generate Event information - Using Match ID
+
+    const event_url = `https://raw.githubusercontent.com/statsbomb/open-data/master/data/events/${match_id}.json`
+    const event_response = await fetch(event_url);
+    const event_object = await event_response.json();
+
+    const [home_team_formation, away_team_formation, home_team_lineup_extracted, away_team_lineup_extracted] = extract_tactical_setups(event_object);
+
+    console.log(home_team_formation);
+    console.log(away_team_formation);
+    console.log(home_team_lineup_extracted);
+    console.log(away_team_lineup_extracted);
+
+    }
 
 getData();
 
